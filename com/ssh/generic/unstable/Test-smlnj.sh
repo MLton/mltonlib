6678b4e64c57f858ab19c2af6cb59fc4c9c370cb
Vesa Karvonen <vesa.a.j.k@gmail>
2007-09-19 13:49:47 +0000

    Script for running tests with SML/NJ.
    SVN r6036

---
+++ com/ssh/generic/unstable/Test-smlnj.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copyright (C) 2007 SSH Communications Security, Helsinki, Finland
+#
+# This code is released under the MLton license, a BSD-style license.
+# See the LICENSE file or http://mlton.org/License for details.
+
+set -e
+set -x
+
+eb=../../extended-basis/unstable
+
+time \
+echo '' | \
+sml -m test.cm \
+    $eb/public/export/{open-top-level.sml,infixes.sml}   \
+    test/utils.sml                                       \
+    $(find test/ -name '*.sml' -a -not -name 'utils.sml')
